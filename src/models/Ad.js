7bf2083f25328a41704a862f057a0837327e50d5
Alireza Asghari <aa9967316@gmail.com>
2024-05-24 15:25:21 +0330

    🗃️ Create Ad model

---
+++ src/models/Ad.js
@@ -0,0 +1,52 @@
+import { models, model, Schema } from "mongoose";
+
+const adSchema = new Schema(
+  {
+    title: {
+      type: String,
+      reqired: true,
+    },
+    description: {
+      type: String,
+      reqired: true,
+    },
+    location: {
+      type: String,
+      reqired: true,
+    },
+    phone: {
+      type: String,
+      reqired: true,
+    },
+    price: {
+      type: Number,
+      reqired: true,
+    },
+    realState: {
+      type: Number,
+      reqired: true,
+    },
+    constructionDate: {
+      type: Date,
+      reqired: true,
+    },
+    category: {
+      type: String,
+      enum: ["villa", "apartment", "store", "earth"],
+      reqired: true,
+    },
+    rules: {
+      type: [String],
+      default: [],
+    },
+    amenities: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Ad = models.Ad || model("Ad", adSchema);
+
+export default Ad;
