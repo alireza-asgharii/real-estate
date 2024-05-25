b5fc1c0e2f174b943a3ae0baa3c6073d57978263
Alireza Asghari <aa9967316@gmail.com>
2024-05-25 16:27:52 +0330

    ✨ Create User Ads page

---
+++ src/app/dashboard/my-ads/page.js
@@ -0,0 +1,7 @@
+import MyAddsPage from "@/components/templates/MyAddsPage";
+
+const AdsPage = () => {
+  return <MyAddsPage />;
+};
+
+export default AdsPage;
---
+++ src/components/templates/MyAddsPage.js
@@ -0,0 +1,9 @@
+const MyAddsPage = () => {
+  return (
+    <div>
+      my add page
+    </div>
+  );
+};
+
+export default MyAddsPage;
