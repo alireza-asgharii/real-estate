1ef7cde84e6ec655457961efa31df153f0424ed3
Alireza Asghari <aa9967316@gmail.com>
2024-05-28 21:59:08 +0330

    ♻️ Optimizing Navlink component for searchParams and change style

--- src/components/modules/NavLink.js
+++ src/components/modules/NavLink.js
@@ -1,15 +1,25 @@
-'use client'
+"use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
-const NavLink = ({ name, href }) => {
+const NavLink = ({ name, href, queryName, queryValue }) => {
   const pathname = usePathname();
-  
+  const searchParams = useSearchParams();
+  const query = searchParams.get(queryName);
+
   return (
     <Link
       href={href}
-      className={`inline-block w-full my-1  p-2 rounded-md ${pathname === href ? 'bg-header-theme text-white' : ''} hover:bg-header-theme hover:opacity-80 hover:text-white transition-colors`}
+      className={`inline-block w-full my-1  p-2 rounded-md ${
+        query
+          ? query === queryValue
+            ? "bg-header-theme text-white"
+            : ""
+          : pathname === href
+          ? "bg-header-theme text-white"
+          : ""
+      } hover:border-header-theme border-2 border-transparent transition-colors`}
     >
       {name}
     </Link>
