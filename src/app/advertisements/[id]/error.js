5e491b67da325af1be7b5bdabd4e36938a092c99
Alireza Asghari <aa9967316@gmail.com>
2024-05-29 20:22:22 +0330

    Use error page for handle when error

---
+++ src/app/advertisements/[id]/error.js
@@ -0,0 +1,9 @@
+"use client";
+
+import ErrorTemplate from "@/components/templates/ErrorTemplate";
+
+const error = () => {
+  return <ErrorTemplate />;
+};
+
+export default error;
---
+++ src/components/templates/ErrorTemplate.js
@@ -0,0 +1,7 @@
+const ErrorTemplate = () => {
+  return (
+    <div className="flex justify-center items-center mt-16">مشکلی پیش آمد</div>
+  );
+};
+
+export default ErrorTemplate;
