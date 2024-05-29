4bebdc2fa60b4f5a9b12dcce06e16ec7b6877089
Alireza Asghari <aa9967316@gmail.com>
2024-05-29 20:17:30 +0330

    Use e2p number function

--- src/utils/numberFormat.js
+++ src/utils/numberFormat.js
@@ -1,5 +1,7 @@
 export const p2e = (s) => s.replace(/[۰-۹]/g, (d) => "۰۱۲۳۴۵۶۷۸۹".indexOf(d));
 
+export const e2p = s => s.replace(/\d/g, d => '۰۱۲۳۴۵۶۷۸۹'[d])
+
 export const priceFormat = (price) => {
   const formated = new Intl.NumberFormat("fa-IR").format(price);
   return formated;
