bd54604e1e112448812c92e40e73e81b55daec70
Alireza Asghari <aa9967316@gmail.com>
2024-05-31 13:52:51 +0330

    Changes in display ads

--- src/app/advertisements/page.js
+++ src/app/advertisements/page.js
@@ -4,7 +4,10 @@ import connectDB from "@/utils/connectDB";
 
 const Advertisements = async ({ searchParams }) => {
   await connectDB();
-  const ads = await Ad.find({ published: true }).select("-userId");
+  const ads = await Ad.find({
+    published: true,
+    status: "verified",
+  }).select("-userId");
   let finall = ads;
   if (searchParams.category) {
     finall = finall.filter((item) => item.category === searchParams.category);
