2776c9f74f1154144816b1164ee617293ba0424b
Alireza Asghari <aa9967316@gmail.com>
2024-05-31 15:11:30 +0330

    💄 Show ad publish status in card

--- src/components/modules/DashboardCard.js
+++ src/components/modules/DashboardCard.js
@@ -3,9 +3,18 @@ import Card from "./Card";
 import DashboardCardButton from "./DashboardCardButton";
 
 const DashboardCard = ({ data }) => {
-  const { _id } = data;
+  const { _id, status } = data;
   return (
-    <div className="w-full sm:w-fit border-2 rounded-md p-2 border-[#BED7DC] hover:border-header-theme hover:shadow-lg transition-colors">
+    <div className="w-full sm:w-fit border-2 rounded-md p-2 border-[#BED7DC] hover:border-header-theme hover:shadow-lg transition-colors relative">
+      {status === "waiting" && (
+        <span className="absolute top-2 left-1 text-xs  rounded-md px-1 py-[2px] bg-gray-500 text-white">در انتظار تایید</span>
+      )}
+      {status === "rejected" && (
+        <span className="absolute top-2 left-1 text-xs  rounded-md px-1 py-[2px] bg-red-500 text-white">رد شده</span>
+      )}
+      {status === "verified" && (
+        <span className="absolute top-2 left-1 text-xs  rounded-md px-1 py-[2px] bg-green-500 text-white">تایید شده</span>
+      )}
       <Link
         title="مشاهده آگهی"
         href="/"
