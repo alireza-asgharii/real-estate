2e88b950fd5e5dcb1035c61e3dbc9bad49c78419
Alireza Asghari <aa9967316@gmail.com>
2024-06-26 19:58:41 +0330

    💄 Hamburger menu for mobile

--- src/components/modules/DashMeButton.js
+++ src/components/modules/DashMeButton.js
@@ -1,5 +1,7 @@
 "use client";
 
+import { IoClose, IoMenu } from "react-icons/io5";
+
 import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -17,6 +19,14 @@ const DashMeButton = () => {
     setOpen(false);
   };
 
+  const menuHandler = (e) => {
+    if (open) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
+
   useEffect(() => {
     if (open) {
       document.body.style.overflow = "hidden";
@@ -27,13 +37,19 @@ const DashMeButton = () => {
 
   if (pathname.startsWith("/dashboard") && status === "authenticated")
     return (
-      <div className="md:hidden" onFocus={openHandler} onBlur={closeHandler} tabIndex="0">
+      <div
+        className="md:hidden flex justify-center items-center"
+        onFocus={openHandler}
+        onBlur={closeHandler}
+        tabIndex="0"
+      >
         <button
-          onClick={openHandler}
-          className=" border-2 px-2 mr-2 rounded-md"
+          onClick={menuHandler}
+          className="text-3xl mr-2 rounded-md inline-block"
         >
-          منو
+          {open ? <IoClose /> : <IoMenu />}
         </button>
+
         <DashboardMenu data={data} open={open} setOpen={setOpen} />
       </div>
     );
